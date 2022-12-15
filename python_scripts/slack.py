3174412a34f571ce18ff7480054106a80b4a300f
lambobr <lambobr1994@gmail.com>
2022-12-15 15:12:48 +0800

    Modified slack.py 0Auth

--- python_scripts/slack.py
+++ python_scripts/slack.py
@@ -4,7 +4,7 @@ try:
 except Exception as e:
     print("Error : {}".format(e))
 
-SLACK_CHANNEL_ID = "C04EPNAC31U"
+SLACK_CHANNEL_ID = "C04FBCV2YGK"
 
-client = WebClient(token="xoxb-4506243522051-4530076132272-LL4bSI9l2BXa8DYaRjWDyK9Q")
+client = WebClient(token="xoxb-4506243522051-4507016124871-IOFmBiNjBpwAcf0K9r53rhZh")
 client.chat_postMessage(channel=SLACK_CHANNEL_ID, text=f"Run successfully: {datetime.now()}")
